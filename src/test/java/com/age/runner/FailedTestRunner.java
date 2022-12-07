bb511d5ffb99e2fa07d2c0532faf2dabf9ffd344
Abdulgani <amirzoev088@gmail.com>
2022-12-07 13:16:36 -0600

    Initial commit

---
+++ src/test/java/com/age/runner/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.age.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/age/step_defs"
+
+)
+public class FailedTestRunner {
+}
---
+++ src/test/java/com/age/runner/Runner.java
@@ -0,0 +1,26 @@
+package com.age.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt" ,
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources" ,
+        glue = "com/age/step_defs",
+        dryRun = false,
+        tags = "@wip"
+)
+
+
+
+public class Runner {
+
+
+}
